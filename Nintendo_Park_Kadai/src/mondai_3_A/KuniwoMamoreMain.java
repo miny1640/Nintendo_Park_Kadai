42c02577e4ddfb372c7e41d0966a6848a289ca5e
LAPTOP-28VPB5JH\pmy <miny1640@naver.com>
2021-03-11 17:00:01 +0900

    インプット（臨時）作成

--- Nintendo_Park_Kadai/src/mondai_3_A/KuniwoMamoreMain.java
+++ Nintendo_Park_Kadai/src/mondai_3_A/KuniwoMamoreMain.java
@@ -5,6 +5,17 @@ public class KuniwoMamoreMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//test
+		int dsNum = 2;
+		String[] ds = {	"3", "3 2 4 6 2 4 1 5 3 5", 
+						"5", "7 4 3 4 6 4 12 3 9 9 2 4 3 7 9 10 2 1 4 12"};
+		
+		String[] answer = new String[dsNum];
+		
+		for(int i = 0 ; i < ds.length ; i++) {
+			System.out.println(ds[i]);
+		}
+		
+		
 
 	}
 
