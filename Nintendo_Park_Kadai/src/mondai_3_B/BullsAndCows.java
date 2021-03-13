6f0131a537c80dc6aa13c9c74a61412a900d8157
LAPTOP-28VPB5JH\pmy <miny1640@naver.com>
2021-03-13 13:01:13 +0900

    メソッドbullsAndCowsが完了したので一回コミット

--- Nintendo_Park_Kadai/src/mondai_3_B/BullsAndCows.java
+++ Nintendo_Park_Kadai/src/mondai_3_B/BullsAndCows.java
@@ -59,23 +59,69 @@ public class BullsAndCows {
 	}
 	
 	static ArrayList<String> bullsAndCows(ArrayList<String> result, String[] ds, int qNum) {
-		if(qNum > 1) {
-			result = bullsAndCows(result, ds, qNum - 1);
-		}
 		
 		String[] question = ds[qNum - 1].split(" ");
 		int bull = 0; //Integer.parseInt(question[1])
 		int cow = 0; //Integer.parseInt(question[2])
-		
-		if(result.size() > 1) {
-			for(int i = 0 ; i < result.size() ; i++) {
-				result.get(i);
-			}
+
+		if(qNum > 1) {
+			result = bullsAndCows(result, ds, qNum - 1);
 		}else {
-			
+			for(int i = 0 ; i < 10 ; i++) {
+				String tmp1 = String.valueOf(i);
+				for(int j = 0 ; j < 10 ; j++) {
+					String tmp2 = tmp1;
+					if(tmp2.contains(String.valueOf(j))) {
+						continue;
+					}else {
+						tmp2 += String.valueOf(j);
+					}
+					for(int k = 0 ; k < 10 ; k++) {
+						String tmp3 = tmp2;
+						if(tmp3.contains(String.valueOf(k))) {
+							continue;
+						}else {
+							tmp3 += String.valueOf(k);
+						}
+						for(int l = 0 ; l < 10 ; l++) {
+							String tmp4 = tmp3;
+							if(tmp4.contains(String.valueOf(l))) {
+								continue;
+							}else {
+								tmp4 += String.valueOf(l);
+								result.add(tmp4);
+							}
+						}
+					}
+				}
+			}
 		}
 		
 		//data check
+		System.out.println("BEFORE");
+		System.out.println("question[0] : " + question[0]);
+		System.out.println("result.size : " + result.size());
+		System.out.println(qNum);
+		
+		if(result.size() >= 1) { //If 'result' has data, filter the data.
+			for(int i = 0 ; i < result.size() ; i++) { //check
+				for(int j = 0 ; j < 4 ; j++) {
+					if(result.get(i).contains(String.valueOf(question[0].charAt(j)))) { //cow? bull?
+						if(result.get(i).charAt(j) == question[0].charAt(j)) { //bull
+							bull++;
+						}else { //cow
+							cow++;
+						}
+					}
+				}
+				if(!(bull == Integer.parseInt(question[1]) && cow == Integer.parseInt(question[2]))) { //filtering
+					result.remove(i);
+				}
+			}
+		}
+		
+		//data check
+		System.out.println("AFTER");
 		System.out.println("question[0] : " + question[0]);
 		System.out.println("result.size : " + result.size());
 		System.out.println(qNum);
